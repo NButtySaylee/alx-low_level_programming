81903e83e9721f1099e854e1a17bd1eda938d3ec
NButtySaylee <nbs917740@outlook.com>
2022-06-06 23:45:58 +0000

    Print lower and upper case using three putchar

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * putchar - print the alphabet in lowercase except and upper case
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+	putchar(ch);
+	putchar(toupper(ch);
+	putchar('\n');
+	return (0);
+}
