68bb4ea3cfc767b65bc5cf43193f2be6d74451cb
NButtySaylee <nbs917740@outlook.com>
2022-06-07 00:10:04 +0000

    Print lower and upper case using three putchar

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,8 +13,8 @@ int main(void)
 	char alp;
 
 	for (alp = 'a'; alp <= 'z'; alp++)
-		putchar(alp);
-		putchar(toupper(alp));
-		putchar('\n');
+	putchar(alp);
+	putchar(toupper(alp));
+	putchar('\n');
 	return (0);
 }
