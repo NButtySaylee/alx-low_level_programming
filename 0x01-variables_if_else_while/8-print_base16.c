0151ea10a82de2e84d80522f6fcd72d70c27d06c
NButtySaylee <nbs917740@outlook.com>
2022-06-07 01:34:49 +0000

    print all the numbers of base 16 in lowercase

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	char b16;
 
-	for (b16 = 30; b16 <= 39; b16++)
+	for (b16 = 48; b16 <= 57; b16++)
 	{
 		putchar(b16);
 	}
-	for (b16 = 70; b16 <= 75; b16++)
+	for (b16 = 97; b16 <= 102; b16++)
 	{
 		putchar(b16);
 	}
