1eb1de1df7286f988f960364fedaca128b2699a1
NButtySaylee <nbs917740@outlook.com>
2022-06-07 01:49:44 +0000

    print all possible different combination of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int numleft;
+	int numright;
+
+
+	for (numleft = 48; numright <= 78; numleft++)
+	{
+		for (numright = numbleft + 1 ; numright <= 78; numright++)
+		{
+			putchar(numleft);
+			putchar (numright);
+
+			if ((numleft == 56) && (numright == 78))
+			{
+				break;
+			}
+
+			putchar(',');
+			putchar (' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
